4a48d956e9e20900b75ae51fcfed0c914ce1fde6
Fabiano Alvarenga <fabiano.alvarenga@corp.globo.com>
2018-02-13 19:31:22 -0200

    Refactor:
    - Substituindo findViewById(...) pelo DataBinding;

--- app/src/main/java/com/udacity/filmesfamosos/DetailMovieActivity.java
+++ app/src/main/java/com/udacity/filmesfamosos/DetailMovieActivity.java
@@ -1,12 +1,14 @@
 package com.udacity.filmesfamosos;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.udacity.filmesfamosos.databinding.ActivityDetailBinding;
 import com.udacity.filmesfamosos.model.dto.PopularMovieDTO;
 import com.udacity.filmesfamosos.service.TheMovieDBService;
 import com.udacity.filmesfamosos.utils.DateUtils;
@@ -19,13 +21,9 @@ public class DetailMovieActivity extends AppCompatActivity {
 
     private PopularMovieDTO popularMovieDTO;
 
-    private TextView tvOriginalTitle;
-    private ImageView imvPoster;
-    private TextView tvReleaseDate;
-    private TextView tvDuration;
-    private TextView tvVoteAverage;
     private Button btFavorite;
-    private TextView tvOverview;
+
+    private ActivityDetailBinding activityDetailBinding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +32,8 @@ public class DetailMovieActivity extends AppCompatActivity {
         this.setTitle(R.string.activity_title_detail);
 
         Intent intent = this.getIntent();
+
+        activityDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
 
         if(null != intent.getExtras()) {
             popularMovieDTO = (PopularMovieDTO) intent.getExtras().get(PopularMovieDTO.POPULAR_MOVIE_DTO);
@@ -44,23 +44,15 @@ public class DetailMovieActivity extends AppCompatActivity {
 
     private void setComponentsValues(PopularMovieDTO popularMovieDTO) {
 
-        tvOriginalTitle = (TextView) findViewById(R.id.tv_originalTitle);
-        tvReleaseDate = (TextView) findViewById(R.id.tv_releaseDate);
-        tvDuration = (TextView) findViewById(R.id.tv_duration);
-        tvVoteAverage = (TextView) findViewById(R.id.tv_vote_average);
-        tvOverview = (TextView) findViewById(R.id.tv_overview);
+        btFavorite = activityDetailBinding.btFavorite;
 
-        imvPoster = (ImageView) findViewById(R.id.imv_poster);
+        activityDetailBinding.tvOriginalTitle.setText(popularMovieDTO.getOriginalTitle());
+        activityDetailBinding.tvReleaseDate.setText(DateUtils.getYearDate(popularMovieDTO.getReleaseDate()));
+        activityDetailBinding.tvDuration.setText("N/I");
+        activityDetailBinding.tvVoteAverage.setText(String.valueOf(popularMovieDTO.getVoteAverage()));
+        activityDetailBinding.tvOverview.setText(popularMovieDTO.getOverview());
 
-        btFavorite = (Button) findViewById(R.id.bt_favorite);
-
-        tvOriginalTitle.setText(popularMovieDTO.getOriginalTitle());
-        tvReleaseDate.setText(DateUtils.getYearDate(popularMovieDTO.getReleaseDate()));
-        tvDuration.setText("N/I");
-        tvVoteAverage.setText(String.valueOf(popularMovieDTO.getVoteAverage()));
-        tvOverview.setText(popularMovieDTO.getOverview());
-
-        TheMovieDBService.processImage(this, popularMovieDTO, imvPoster);
+        TheMovieDBService.processImage(this, popularMovieDTO, activityDetailBinding.imvPoster);
 
     }
 }
